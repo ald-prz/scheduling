96a84432911428774c9dd1d9e01db27f2f947ce8
przemek <Przemek>
2016-10-10 20:05:49 +0200

    Fix error format in Input Recognizer

--- generator/input_recognizer.cpp
+++ generator/input_recognizer.cpp
@@ -7,17 +7,15 @@ Configuration* InputRecognizer::Recognize(int argc, char *argv[])
     Configuration *result;
 
     if (argc < 7)
-        throw ExceptionInput("-u flag is missing");
+        throw ExceptionInput("-u flag is missing\n");
     else if (strcmp(argv[1], "-u") != 0)
-        throw ExceptionInput("-u flag is missing");
+        throw ExceptionInput("-u flag is missing\n");
     else if (strcmp(argv[3], "-n") != 0)
-        throw ExceptionInput("-n flag is missing");
+        throw ExceptionInput("-n flag is missing\n");
     else if (strcmp(argv[5], "-o") != 0)
-        throw ExceptionInput("-o flag is missing");
+        throw ExceptionInput("-o flag is missing\n");
     else
-    {
         result = new Configuration(atof(argv[2]), atoi(argv[4]), argv[6]);
-    }
 
     return result;
 }
